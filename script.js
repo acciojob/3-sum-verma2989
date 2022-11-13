0ed7622fd9e926d96220f8b0a7cf524a7c21e331
Krishna Kumar <105854880+verma2989@users.noreply.github.com>
2022-11-13 21:55:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function threeSum(arr, target) {
+   let closestSum = Number.MAX_VALUE;
+ 
+    
+    for(let i = 0; i < arr.length ; i++)
+    {
+        for(let j =i + 1; j < arr.length; j++)
+        {
+            for(let k =j + 1; k < arr.length; k++)
+            {
+                 
+                // Update the closestSum
+                if (Math.abs(target - closestSum) >
+                    Math.abs(target - (arr[i] + arr[j] + arr[k])))
+                    closestSum = (arr[i] + arr[j] + arr[k]);
+            }
+        }
+    }
+     
+    // Return the closest sum found
+    return closestSum;
   //your code here
 }
 
